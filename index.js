491ee44e695a1080bdc5c9690380d7363b7e2c1d
AshisheKumar <ashishkumarr1108@gmail.com>
2023-09-13 22:23:27 +0530

    complete

--- index.js
+++ index.js
@@ -9,36 +9,12 @@ const jwt = require('jsonwebtoken');
 app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
-const secretekey = "dffsdfdvsdfdgvgvsdfdb";
-
-function authenticateToken(req, res, next) {
-
-    var loggeduser = req.cookies.user;
-    if(loggeduser){
-        next();
-    }
-    else{
-        var token = req.body.token;
-        if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-        }
-    
-        jwt.verify(token, secretekey, (err, user) => {
-        if (err) {
-            return res.status(403).json({ message: 'Forbidden' });
-        }
-        req.user = user;
-        next();
-        });
-    }
-  }
-
-
 
 // In-memory data store
 let posts = [
   {
     id: 1,
+    userid:3,
     title: "What is Lorem Ipsum?",
     content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamcoDecentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
     author: "Lorem paul",
@@ -46,6 +22,7 @@ let posts = [
   },
   {
     id: 2,
+    userid:5,
     title: "TNisl nisi scelerisques",
     content:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id nibh tortor id aliquet lectus proin nibh. Id donec ultrices tincidunt arcu non. Molestie at elementum eu facilisis. ",
@@ -54,6 +31,7 @@ let posts = [
   },
   {
     id: 3,
+    userid:10,
     title: "Dignissim diam",
     content:"Magna eget est lorem ipsum dolor sit amet. Urna id volutpat lacus laoreet non curabitur gravida arcu. Nunc scelerisque viverra mauris in aliquam sem.",
      author: "Samuel Green",
@@ -64,17 +42,23 @@ let posts = [
 let lastId = 3;
 
 // Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 //Write your code here//
 
 
 //CHALLENGE 1: GET All posts
 
-app.get("/posts",(req,res)=>{
-  console.log(posts);
-  res.json(posts);
+app.get("/getposts",(req,res)=>{
+  let userid = parseInt(req.query.userid);
+  let userposts = [];
+  posts.forEach(v=>{
+    if (v.userid === userid){
+      userposts.push(v);
+    }  
+  })
+  //console.log(posts);
+  res.json(userposts);
 });
 
 //CHALLENGE 2: GET a specific post by id
@@ -93,25 +77,30 @@ app.get("/posts/:id/",(req,res)=>{
 
 //CHALLENGE 3: POST a new post
 
-app.post("/posts",(req,res)=>{
+app.post("/newpost",(req,res)=>{
   const newId = posts.length+1;
   const newPost={
     id : newId,
+    userid:parseInt(req.body.userid),
     title : req.body.title,
     author : req.body.author,
+    content:req.body.content,
     date : new Date(),
   };
     
   posts.push(newPost);
-  res.status(201).json(newPost);
+  res.status(201).send();
 });
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
 
-app.patch("/posts/:id/",(req,res) => {
-
+app.post("/editposts/:id/",(req,res) => {
+  let userid = req.body.userid;
   const foundBlog = posts.find((blog)=>blog.id===parseInt(req.params.id));
   if(!foundBlog){
+    return res.status(404).json({ message: "Post not found" });
+  }
+  if(foundBlog.userid != userid){
     return res.status(404).json({ message: "Post not found" });
   }
   if(req.body.title){
@@ -123,36 +112,44 @@ app.patch("/posts/:id/",(req,res) => {
   if(req.body.content){
     foundBlog.content = req.body.content;
   }
-
+  console.log(foundBlog);
   res.json(foundBlog);
 });
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
 
-app.delete("/posts/:id/",(req,res)=>{
-
+app.post("/deleteposts/:id/",(req,res)=>{
   const foundIndex = posts.findIndex((blog)=>blog.id === parseInt(req.params.id));
   if(foundIndex===-1)
   {
     return res.status(404).json({ message: "Post not found" });
   }
+  else if( req.body.userid != posts[foundIndex].userid){
+    return res.status(404).json({ message: "Post not found" });
+  }
   else{
     posts.splice(foundIndex, 1);
-  res.json({ message: "Post deleted" });
+    res.json({ message: "Post deleted" });
   }
 
 });
 
 
-app.get("/login",(req,res)=>{
-  var token = jwt.sign({userid:"1"},secretekey);
-  res.render("login",{token:token});
-});
+// app.get("/posts", authenticateCRUD, (req, res) => {
+//   // Render your blog posts page
+//   // res.render("index", { posts: posts });
+//   res.json(posts);
 
-app.post("/login",authenticateToken,(req,res)=>{
-  res.cookie('user', req.user.userid);
-  res.render("index",{posts:users});
-});
+// });
+
+
+
+// app.post("/login",authenticateToken,(req,res)=>{
+//   var token = jwt.sign({userid:"1"},secretekey);
+//   res.cookie('user', token);
+//   res.render("index",{posts:posts});
+// });
+
 
 
 
--- server.js
+++ server.js
@@ -1,36 +1,110 @@
-import express from "express";
-import bodyParser from "body-parser";
-import axios from "axios";
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios")
 const app = express();
+const cookieParser = require("cookie-parser");
+const jwt = require('jsonwebtoken');
+
+app.set('view engine', 'ejs');
+app.use(cookieParser());
 const port = 3000;
 const API_URL = "http://localhost:4000";
 
-app.use(express.static("public"));
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const users = require("./MOCK_DATA.json");
+const secretekey = "dffsdfdvsdfdgvgvsdfdb";
+
+function authenticateToken(req, res, next) {
+
+    var loggeduser = req.cookies.user;
+    if(loggeduser){
+      jwt.verify(loggeduser, secretekey, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+        req.user = parseInt(user.id);
+        next();
+        });
+    }
+    else{
+        var token = req.token;
+        if (!token) {
+        // return res.status(401).json({ message: 'Unauthorized' });
+        res.redirect("/login");
+        }
+    
+        jwt.verify(token, secretekey, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+        req.user = parseInt(user.id);
+        next();
+        });
+    }
+  }
+
+ 
+ 
+app.get("/",(req,res)=>{
+  if(req.cookies.user){
+    res.redirect("/showposts");
+  }
+  else{
+    res.redirect("/login");
+  }
+})
+
+  app.get("/login",(req,res)=>{
+    
+    res.render("login");
+  });
+
+  app.post("/login" ,(req, res) => {
+    f_name = req.body.first_name;
+    l_name = req.body.last_name;
+    email = req.body.email;
+    var found_user = null;
+    for (let i = 0; i < users.length; i++) {
+      const element = users[i];
+      if(element.email === email && element.last_name === l_name){
+        found_user = element;
+        break;
+      }
+    }
+    if(found_user === null){
+      res.send("User not found");
+    }
+    var token = jwt.sign(found_user,secretekey);
+    
+    res.cookie('user', token);
+    res.redirect("/showposts");
+  });
+  
+
 // Route to render the main page
-app.get("/", async (req, res) => {
+app.get("/showposts",authenticateToken, async (req, res) => {
+  let id  = req.user;
   try {
-    const response = await axios.get(`${API_URL}/posts`);
-    console.log(response);
+    const response = await axios.get(`${API_URL}/getposts?userid=${id}`);
+    //console.log(response);
     res.render("index.ejs", { posts: response.data });
   } catch (error) {
-    res.status(500).json({ message: "Error fetching posts" });
+    res.status(500).json({ message: error });
   }
 });
 
 // Route to render the edit page
-app.get("/new", (req, res) => {
+app.get("/new",authenticateToken, (req, res) => {
   res.render("modify.ejs", { heading: "New Post", submit: "Create Post" });
 });
 
-app.get("/edit/:id", async (req, res) => {
+app.get("/edit/:id",authenticateToken, async (req, res) => {
   try {
     const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
-    console.log(response.data);
     res.render("modify.ejs", {
       heading: "Edit Post",
       submit: "Update Post",
@@ -42,9 +116,14 @@ app.get("/edit/:id", async (req, res) => {
 });
 
 // Create a new post
-app.post("/api/posts", async (req, res) => {
+app.post("/api/posts",authenticateToken, async (req, res) => {
   try {
-    const response = await axios.post(`${API_URL}/posts`, req.body);
+    req.body.userid = req.user;
+    const response = await axios.post(`${API_URL}/newpost`,req.body,
+    {headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }},  
+    );
     console.log(response.data);
     res.redirect("/");
   } catch (error) {
@@ -53,29 +132,42 @@ app.post("/api/posts", async (req, res) => {
 });
 
 // Partially update a post
-app.post("/api/posts/:id", async (req, res) => {
-  console.log("called");
+app.post("/api/posts/:id",authenticateToken, async (req, res) => {
   try {
-    const response = await axios.patch(
-      `${API_URL}/posts/${req.params.id}`,
-      req.body
+    let userid = req.user;
+    req.body.userid = userid;
+    const response = await axios.post(
+      `${API_URL}/editposts/${req.params.id}`,
+      req.body,{headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }},
     );
-    console.log(response.data);
+   console.log(response.data);
     res.redirect("/");
   } catch (error) {
-    res.status(500).json({ message: "Error updating post" });
+    res.status(500).json({ message: error });
   }
 });
 
 // Delete a post
-app.get("/api/posts/delete/:id", async (req, res) => {
+app.get("/api/posts/delete/:id",authenticateToken, async (req, res) => {
   try {
-    await axios.delete(`${API_URL}/posts/${req.params.id}`);
+    let userid = req.user;
+    await axios.post(`${API_URL}/deleteposts/${req.params.id}`,{"userid":userid},
+    {headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }},
+    );
     res.redirect("/");
   } catch (error) {
     res.status(500).json({ message: "Error deleting post" });
   }
 });
+
+app.get("/logout",authenticateToken,(req,res)=>{
+  res.clearCookie("user");
+  res.redirect("/login");
+})
 
 app.listen(port, () => {
   console.log(`Backend server is running on http://localhost:${port}`);
